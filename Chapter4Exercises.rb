dbd687fce5fe0b26ed89ac66844c06c26d92c9c1
Melissa L <circusgirl06@gmail.com>
2013-09-16 18:31:12 -0400

    first commit

---
+++ Chapter4Exercises.rb
@@ -0,0 +1,40 @@
+# Chapter 4: Variables and Assignment
+
+my_string = '...you can say that again...';
+puts my_string;
+puts my_string;
+
+puts '';
+
+name = 'Anya Christina Emmanuella Jenkins Harris';
+puts 'My name is ' + name + '.';
+puts 'Wow! ' + name;
+puts 'is a really long name!';
+puts '';
+
+# Variable reassignment 
+
+composer = 'Mozart';
+puts composer + ' was "da bomb" in his day.';
+
+composer = 'Beethoven';
+puts 'But I prefer ' + composer + ', personally.';
+puts '';
+
+my_own_var = 'just another ' + 'string';
+puts my_own_var;
+
+my_own_var = 5 * (1+2); # Variables can point to anything except other variables
+puts my_own_var;
+puts '';
+
+var1 = 8;
+var2 = var1;
+puts var1;
+puts var2;
+
+puts '';
+
+var1 = 'eight';
+puts var1;
+puts var2;
